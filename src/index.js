36b5747d1ffac383984ba8af2b8eb2d2abf5beaa
markr155 <markricho155@gmail.com>
2023-11-15 23:09:57 +1100

    formatting and styling complete

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import "../style/styles.css"
+import "../style/styles.css";
 
 console.clear();
-const test = document.querySelector('.test');
+const test = document.querySelector(".test");
 
 function getWeather(location) {
-    const weatherInfo = {};
-    fetch(`https://api.weatherapi.com/v1/current.json?key=0d23b12804e64ad0b8673526231411&q=${location}`, 
+  const weatherInfo = {};
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=0d23b12804e64ad0b8673526231411&q=${location}`,
     {
-        mode: 'cors',
-        method: 'get',
-    })
-    .then((response) => response.json()
-    .then((resp) => {
-        weatherInfo.name = resp.location.name;
-        return weatherInfo;
-    }))
+      mode: "cors",
+      method: "get",
+    },
+  ).then((response) =>
+    response.json().then((resp) => {
+      weatherInfo.name = resp.location.name;
+      return weatherInfo;
+    }),
+  );
 }
 
-const x = getWeather('traralgon');
+const x = getWeather("traralgon");
